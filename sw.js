7d75df9a1d0968352f7630633bf4b53795c2890b
Jeroen van Tilburg <jvantilburg@gmail.com>
2021-05-06 10:52:24 +0200

    Added new favicon and service worker

---
+++ sw.js
@@ -0,0 +1,54 @@
+var CACHE_NAME = 'motionticker-cache-v1';
+var CURRENT_CACHES = CACHE_NAME;
+var urlsToCache = [
+  './',
+  'index.html',
+  'style.css',
+  'favicon.ico',
+  'scripts/fabric.min.js',
+  'scripts/jquery-3.5.0.min.js',
+  'scripts/whiteboard.js',
+  'scripts/registerSW.js',
+  'img/wit_rooster_klein.png',
+  'img/zwart_rooster_klein.png',
+  'apple-touch-icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.eot',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.svg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2',
+  'mstile-70x70.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  // Return without calling event.respondWith()
+  // if this is a range request.
+  if (event.request.headers.has('range')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
